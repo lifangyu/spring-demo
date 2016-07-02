36252383b453d84a425fd65dd236c764caade5f0
Lify <914101613@qq.com>
2016-07-02 15:59:31 +0800

    # 单元测试

---
+++ spring-generator/auth-manage/src/test/java/com/auth/controller/BaseControllerTest.java
@@ -0,0 +1,35 @@
+package com.auth.controller;
+
+
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
+
+import junit.framework.TestCase;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration(locations = { "classpath:spring/applicationContext.xml", "classpath:spring/spring-mvc.xml" })
+public class BaseControllerTest extends TestCase {
+	protected Logger logger = LoggerFactory.getLogger(BaseControllerTest.class);
+
+	@Autowired
+	private WebApplicationContext wac;
+
+	protected MockMvc mockMvc;
+
+	@Before
+	public void setup() {
+		this.mockMvc = webAppContextSetup(this.wac).build();
+	}
+
+}
