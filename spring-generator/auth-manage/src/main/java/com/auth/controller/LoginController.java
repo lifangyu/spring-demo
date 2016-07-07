078f1f29a122bd280b1ef18dd14d47c22b910e49
Lify <914101613@qq.com>
2016-07-07 17:01:13 +0800

    # 菜单级联处理

--- spring-generator/auth-manage/src/main/java/com/auth/controller/LoginController.java
+++ spring-generator/auth-manage/src/main/java/com/auth/controller/LoginController.java
@@ -3,15 +3,29 @@ package com.auth.controller;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 
 import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.auth.controller.service.LoginService;
+import com.auth.model.AuthMenu;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.spring.common.utils.entity.ZTreeNode;
 import com.spring.common.utils.image.ImageUtils;
 
 /**
@@ -21,14 +35,18 @@ import com.spring.common.utils.image.ImageUtils;
  * @version V1.0
  */
 @Controller
+@RequestMapping(value = "login")
 public class LoginController {
+
+    @Autowired
+    private LoginService loginService;
 	
 	/**
 	 * 生成验证码图片
 	 * @throws IOException 
 	 */
-	@RequestMapping("/image/createImage")
-	public void createImage(HttpServletResponse response,HttpSession session) throws IOException {
+    @RequestMapping(value = "createImage", method = RequestMethod.GET)
+    public void createImage(HttpServletResponse response, HttpSession session) throws IOException {
 		//生成验证码及图片
 		Map<String, BufferedImage> map = ImageUtils.createImage();
 		//取得验证码，存入session
@@ -44,8 +62,150 @@ public class LoginController {
 		} catch(IOException ie){
 		    throw ie;
 		}finally {
-		    os.close();
+            if (os != null)
+                os.close();
         }
 	}
+
+    /**
+     * 生成验证码图片
+     * @throws IOException 
+     */
+    @RequestMapping(value = "checkLogin", method = RequestMethod.POST)
+    @ResponseBody
+    public int checkLogin(String username, String code, String pass, HttpSession session) throws IOException {
+        // 验证验证码
+        String imageCode = (String) session.getAttribute("imageCode");
+        if (code == null || !code.equalsIgnoreCase(imageCode)) {
+            // 验证码输入有误
+            return 1;
+        }
+        return 0;
+    }
+
+
+    /**
+     * 系统首页
+     * @throws IOException 
+     */
+    @RequestMapping(value = "main", method = RequestMethod.GET)
+    public String sysMain() throws IOException {
+        // 进入pages/main.jsp页面
+        return "main";
+    }
+
+    /**
+     * tree 菜单
+     */
+    @SuppressWarnings("unused")
+    @ResponseBody
+    @RequestMapping(value = "treeView")
+    public String treeView(HttpServletRequest request){
+        Long userId = 0L;
+        // 父节点
+        List<ZTreeNode> nodeList = new ArrayList<ZTreeNode>();
+        ZTreeNode treeNode = null;
+        Comparator<ZTreeNode> comparator = new Comparator<ZTreeNode>() {
+            @Override
+            public int compare(ZTreeNode o1, ZTreeNode o2) {
+                ZTreeNode p1 = (ZTreeNode) o1;
+                ZTreeNode p2 = (ZTreeNode) o2;
+                if (p1.getSort() == null || p2.getSort() == null) {
+                    return Integer.parseInt(p1.getId()) - Integer.parseInt(p2.getId());
+                } else {
+                    if (p1.getSort() > p2.getSort()) {
+                        return 1;
+                    } else {
+                        return -1;
+                    }
+                }
+            }
+        };
+
+        List<AuthMenu> menus = loginService.getMenusByUserIdPid(userId, 0L);
+        nodeList = getMenuJsonObject(userId, menus);
+        // 排序，因为从set里取出来的值是无序的
+        Collections.sort(nodeList, comparator);
+        try {
+            System.out.println(new ObjectMapper().writeValueAsString(nodeList));
+            return new ObjectMapper().writeValueAsString(nodeList);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    /**
+     *
+     * @author lifangyu
+     * @param menus
+     * @return
+     * 		List<ZTreeNode>
+     */
+    private List<ZTreeNode> getMenuJsonObject(long userId, List<AuthMenu> menus) {
+        List<ZTreeNode> nodeList = new ArrayList<>();// 菜单跟目录集合
+        for (Iterator<AuthMenu> iterator = menus.iterator(); iterator.hasNext();) {
+            AuthMenu authMenu = (AuthMenu) iterator.next();
+            // 一级菜单对象
+            ZTreeNode node = new ZTreeNode(String.valueOf(authMenu.getId()), String.valueOf(authMenu.getParentId()),
+                    authMenu.getMenuName(), authMenu.getFolder() == 1 ? "true" : "false", authMenu.getIconUrl(),
+                    authMenu.getSort());
+
+            if (authMenu.getFolder() == 1) {
+                List<AuthMenu> menuChileds = loginService.getMenusByUserIdPid(userId, authMenu.getId());
+                List<ZTreeNode> nodeChileList = new ArrayList<>();
+                for (Iterator<AuthMenu> iteratorsec = menuChileds.iterator(); iteratorsec.hasNext();) {
+                    AuthMenu authMenuChiled = (AuthMenu) iteratorsec.next();
+                    // 一级菜单下的子菜单对象(二级菜单)
+                    ZTreeNode sec = new ZTreeNode(String.valueOf(authMenuChiled.getId()),
+                            String.valueOf(authMenuChiled.getParentId()), authMenuChiled.getMenuName(),
+                            authMenuChiled.getFolder() == 1 ? "true" : "false", authMenuChiled.getIconUrl(),
+                            authMenuChiled.getSort());
+                    if (authMenuChiled.getFolder() == 1) {
+                        // 二级菜单下的子菜单(三级菜单)
+                        sec.setChildren(this.getMenuJsonObjectSec(userId, authMenuChiled));
+                    }
+                    nodeChileList.add(sec);
+                }
+                node.setChildren(nodeChileList);
+            } else {
+                nodeList.add(new ZTreeNode(String.valueOf(authMenu.getId()), String.valueOf(authMenu.getParentId()),
+                        authMenu.getMenuName(), authMenu.getFolder() == 1 ? "true" : "false", authMenu.getIconUrl(),
+                        authMenu.getSort()));
+            }
+            nodeList.add(node);
+        }
+
+        return nodeList;
+    }
+
+    /**
+     * 级联菜单 递归处理
+     *
+     * @author lifangyu
+     * @param nodeList 
+     * @param userId
+     * @param authMenuChiled
+     * @return
+     * 		List<ZTreeNode>
+     */
+    private List<ZTreeNode> getMenuJsonObjectSec(long userId, AuthMenu authMenuChiled) {
+        List<ZTreeNode> nodeList = new ArrayList<>();
+        List<AuthMenu> menuChileds = loginService.getMenusByUserIdPid(userId, authMenuChiled.getId());
+        for (Iterator<AuthMenu> iterators = menuChileds.iterator(); iterators.hasNext();) {
+            AuthMenu authMenuThird = (AuthMenu) iterators.next();
+            // 三级菜单对象
+            ZTreeNode third = new ZTreeNode(String.valueOf(authMenuThird.getId()),
+                    String.valueOf(authMenuThird.getParentId()), authMenuThird.getMenuName(),
+                    authMenuThird.getFolder() == 1 ? "true" : "false", authMenuThird.getIconUrl(),
+                    authMenuThird.getSort());
+            if (authMenuThird.getFolder() == 1) {
+                // 三级菜单对象下的子菜单(以此递归)
+                third.setChildren(this.getMenuJsonObjectSec(userId, authMenuThird));
+            }
+            nodeList.add(third);
+        }
+        return nodeList;
+    }
 
 }
--- spring-generator/auth-manage/src/main/webapp/pages/main.jsp
+++ spring-generator/auth-manage/src/main/webapp/pages/main.jsp
@@ -1,116 +1,71 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
-<%@include file="/pages/common/global.jsp"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="utf-8"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>权限管理系统</title>
+<head id="Head1">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>管理系统</title>
+    <%@include file="/pages/common/global.jsp"%>
+    <script type="text/javascript" src="<%=request.getContextPath() %>/static/easyui/menuStandardData.js"></script>
+	<script type="text/javascript" src="<%=request.getContextPath() %>/static/js/leftMenu.js"></script>
+
 </head>
-<body class="easyui-layout" >
-<!--头部begin-->
-<div region="north" border="true" split="false" style="height:15%;overflow:hidden;" href="<c:url value='/pages/head.jsp' />"></div>
-<!--头部end-->
-
-<!-- 左侧菜单begin  -->
-<div region="west" title="系统菜单" split="true" style="width: 12%;">
- 	<div style="margin: 10px 0;">
-			<ul class="easyui-tree" id="treeView"></ul>
+<body class="easyui-layout" style="overflow-y: hidden"  fit="true"   scroll="no">
+<noscript>
+<div style=" position:absolute; z-index:100000; height:2046px;top:0px;left:0px; width:100%; background:white; text-align:center;">
+    <img src="../static/images/noscript.gif" alt='抱歉，请开启脚本支持！' />
+</div></noscript>
+	<!-- head 部 -->
+    <div region="north" split="false" border="false" style="overflow: hidden; height: 30px;
+        background: url(../static/images/layout-browser-hd-bg.gif) #7f99be repeat-x center 50%;
+        line-height: 20px;color: #fff; font-family: Verdana, 微软雅黑,黑体">
+        <span style="float:right; padding-right:20px;" class="head">欢迎 admin <a href="#" class="easyui-linkbutton" onClick="load2()">修改密码</a> <a href="#" class="easyui-linkbutton" onClick="load2()">安全退出</a></span>
+        <span style="padding-left:10px; font-size: 16px; "><img src="../static/images/blocks.gif" width="20" height="20" align="absmiddle" /> jQuery.EasyUI- 1.4.4 + zTree3.5.19 完美结合 应用实例</span>
+    </div>
+    
+    <!-- foot 底部 -->
+    <div region="south" split="false" style="height: 30px; background: #D2E0F2; ">
+        <div class="footer">lify版权所有</div>
+    </div>
+    
+    <!-- 左侧菜单 -->
+    <div region="west" split="true" title="导航菜单" style="width:180px;" id="west">
+		<div class="easyui-accordion" style="width: 200px; height: 300px;" fit=true border=false>
+			<div title="oa系统菜单">
+				<ul id="oa_menu" class="ztree"></ul>
+			</div>
+        	<div title="jQ.EasyUI示例">
+				<ul id="treeDemo1" class="ztree"></ul>
+			</div>
+			<div title="zTree 示例" fit=true border=false>
+				<ul id="treeDemo2" class="ztree"></ul>
+			</div>
+        </div>
+    </div>
+    
+    <!-- 欢迎主页 -->
+    <div id="mainPanle" region="center" style="background:#eee;overflow-y:hidden">
+        <div id="tabs" class="easyui-tabs"  fit="true" border="false">
+			<div title="首页" style="padding:20px;overflow:hidden;color:red;" >
+				<h1 style="font-size:24px;">* Edit By CYY</h1>
+                <h1 style="font-size:24px;">* jQuery.EasyUI- 1.4.4 + zTree3.5.19 完美结合 应用实例 </h1>
+                <h1 style="font-size:24px;">* </h1>
+				<h1 style="font-size:24px;">* 2016-06-06</h1>
+			</div>
+		</div>
+    </div>
+	<!-- tab 上动态菜单 -->
+	<div id="right-hand_menu" class="easyui-menu" style="width:150px;">
+		<div id="tabupdate">刷新</div>
+		<div class="menu-sep"></div>
+		<div id="close">关闭</div>
+		<div id="closeall">全部关闭</div>
+		<div id="closeother">除此之外全部关闭</div>
+		<div class="menu-sep"></div>
+		<div id="closeright">当前页右侧全部关闭</div>
+		<div id="closeleft">当前页左侧全部关闭</div>
+		<div class="menu-sep"></div>
+		<div id="exit">退出</div>
 	</div>
-</div>
-<!-- 左侧菜单end  -->
 
-<!--中间内容begin-->
-<div region="center">
-	<div id="main-panle" class="easyui-tabs" fit="true" border="false">
-		<div title="首页" id="tabs" href="<c:url value='/pages/welcome.jsp' />"></div>
-	</div>
-</div>
-<!--中间内容end-->
-
-<!-- 底部begin-->
-<div region="south" border="false" split="false" style="height:4%;overflow:hidden;" href="<c:url value='/pages/foot.jsp'/>"></div>
- <!-- 底部end-->
- 
 </body>
 </html>
-<script type="text/javascript">
-<!-- 注： 加载菜单独立成一个js文件不好用 -->
-var _treeView = $('#treeView');//菜单栏
-var _centerTabs = $('#tabs');//选项卡栏
-$(function(){
-	//加载左侧菜单树
-	_treeView.tree({
-		 checkbox: false, 			 
-		 url:'${ctx}login/treeView?pid=0',
-		 onBeforeExpand:function(node){
-			 _treeView.tree("options").url="${ctx}login/treeView?pid="+node.id;
-		 },
-		 onClick:function(node){
-			 var click_obj = $('#treeView').tree('getSelected');
-			 if(click_obj.state == 'open' && click_obj.attributes == null){
-				 _treeView.tree('collapse',node.target);
-			 }else if(click_obj.state == 'closed' && click_obj.attributes == null){
-				 _treeView.tree('expand',node.target);
-			 }else{
-				    var _tit = node.text;
-					var _contant = node.attributes["url"];
-				    var _tabCls = node.iconCls;
-					addNewTab(_tit, _contant, _tabCls, node.id);
-			 }
-		 }
-	}); 
-	//给菜单添加点击事件
-	$('.easyui-tree a').click(function() {
-	    var _tit = $(this).html(),
-	    _contant = $(this).attr("href"),
-	    _tabCls = $(this).attr("tabCls");
-	    addNewTab(_tit, _contant, _tabCls, $(this).attr("id"));
-	    return false;
-	});
-	
-	//加载选项卡
-	_centerTabs.tabs({
-		fit : true,
-		border : false,
-		onContextMenu : function(e, title) {
-			e.preventDefault();
-			tabsMenu.menu('show', {
-				left : e.pageX,
-				top : e.pageY
-			}).data('tabTitle', title);
-		}
-	});	
-	
-	//添加选项卡
-	function addNewTab(title, href, tabCls, nodeId){
-		 //选项卡索引
-		 var index;
-		 //选择存在的选项卡
-		 $(_centerTabs.tabs('tabs')).each(function(i){
-			 if(this[0].id == 'tree'+nodeId){
-				 index = _centerTabs.tabs('getTabIndex',this);
-				 _centerTabs.tabs('select', index);
-				 return false;
-			 }
-		 });
-		 
-		 //不存在，则创建选项卡
-		if(!_centerTabs.tabs('exists',index)){
-			var content = 'no load page!';
-			if(Boolean(href)){
-				content = '<iframe scrolling="no" frameborder="0"  src="'+ href + '" style="width:100%;height:99%;"></iframe>';
-			}
-			_centerTabs.tabs({
-				scrollIncrement : 100
-			}).tabs('add', {
-				id : 'tree'+nodeId,
-				title : title,
-				content : content,
-				closable : true,
-				iconCls : tabCls
-			});
-		}
-	 }
-});
-</script>
